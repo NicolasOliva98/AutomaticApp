d3c0cb71b461c64a6e7fe80ea0f2cef5d2600c77
Lordfarkuuad <oliva.nicolas30@gmail.com>
2020-11-06 18:19:33 -0300

    Custom DrawerNavigation

--- src/Routes.js
+++ src/Routes.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import Text from 'react-native'
+import Text, { Dimensions } from 'react-native'
 import { createAppContainer, createSwitchNavigator } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
-import{ createDrawerNavigator } from 'react-navigation-drawer'
+import { createDrawerNavigator, DrawerItems } from 'react-navigation-drawer'
 import AuthLoading from './screens/AuthLoading'
 import HomeScreen from './screens/Home'
 import LoginScreen from './screens/Login'
@@ -12,20 +12,23 @@ import BluetoothScreen from './screens/Bluetooth'
 import MainScreen from './screens/Main'
 import FirtsScreen from './screens/firts'
 
+import Icon from 'react-native-vector-icons/Ionicons'
+import SideBar from './components/SideBar'
 
 if (Text.defaultProps == null) Text.defaultProps = {};
 Text.defaultProps.allowFontScaling = false;
 
+const sizeIcon = 25;
 
 const OnboardingNavigator = createStackNavigator({
-  Firts:{
+  Firts: {
     screen: FirtsScreen,
   },
   Login: {
     screen: LoginScreen,
   },
   Register: {
-    screen: RegisterScreen, 
+    screen: RegisterScreen,
   },
 }, {
   initialRouteName: 'Register',
@@ -33,28 +36,53 @@ const OnboardingNavigator = createStackNavigator({
 })
 
 const AppNavigator = createDrawerNavigator({
-    Home: {
-        screen: HomeScreen,
-
-    },
-    Perfil:{
-      screen: PerfilScreen,
-    },
-    Bluetooth:{
-      screen: BluetoothScreen,
+  Home: {
+    screen: HomeScreen,
+    navigationOptions: {
+      title: 'Inicio',
+      drawerIcon: ({ tintColor }) => <Icon name='home' color={tintColor} size={sizeIcon} />
     }
-},{
-    initialRouteName: 'Home',
-    headerMode: 'none'
+
+  },
+  Bluetooth: {
+    screen: BluetoothScreen,
+    navigationOptions: {
+      title: 'Riego',
+      drawerIcon: ({ tintColor }) => <Icon name='home' color={tintColor} size={sizeIcon} />
+    }
+  },
+  Perfil: {
+    screen: PerfilScreen,
+    navigationOptions: {
+      title: 'Mi Perfil',
+      drawerIcon: ({ tintColor }) => <Icon name='home' color={tintColor} size={sizeIcon} />
+    }
+  }
+}, {
+  contentComponent: props => <SideBar {...props} />,
+  drawerWidth: Dimensions.get('window').width * 0.70,
+  contentOptions: {
+    activeBackgroundColor: 'rgba(113,222,215,0.15)',
+    activeTintColor: '#0f4c75',
+    inactiveTintColor: '#4b5d67',
+    itemsContainerStyle: {
+      marginTop: 16,
+      marginHorizontal: 8
+    },
+    itemStyle: {
+      borderRadius: 10
+    }
+  }
+
 })
 
 const BaseStack = createSwitchNavigator({
-    AuthLoading,
-    Onboarding: OnboardingNavigator,
-    Root: AppNavigator
-  },{
-      initialRouteName: 'AuthLoading'
-  })
+  AuthLoading,
+  Onboarding: OnboardingNavigator,
+  Root: AppNavigator
+}, {
+  initialRouteName: 'AuthLoading'
+})
 
 
 export default createAppContainer(BaseStack)
---
+++ src/components/SideBar.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react'
+import { View, Text, ScrollView, ImageBackground, Image, StyleSheet } from 'react-native'
+import { DrawerItems } from 'react-navigation-drawer';
+import AsyncStorage from '@react-native-community/async-storage'
+
+export default props => {
+
+    const [loading, setLoading] = useState(true)
+    const [user, setUser] = useState([])
+    const fetchUser = async () => {
+        const x = await AsyncStorage.getItem('token')
+        const res = await fetch('https://servelessautomatic.vercel.app/api/auth/me', { method: 'GET', headers: { 'Content-Type': 'application/json', authorization: x }, })
+        const data = await res.json()
+        setUser(data)
+        setLoading(false)
+    }
+    useEffect(() => {
+        fetchUser()
+    }, [])
+
+    return (
+        <ScrollView>
+            <ImageBackground
+                blurRadius={10}
+                source={{uri:'https://wallpapertag.com/wallpaper/full/e/a/1/135335-firewatch-background-1920x1080-windows-10.jpg'}} style={{ width: undefined, padding: 16, paddingTop: 48 }}>
+                <Image
+                    style={styles.image}
+                    source={{uri: user.foto}}
+                />
+                <Text style={styles.Textname}>{user.nombre}</Text>
+            </ImageBackground>
+            <View style={styles.container}>
+                <DrawerItems {...props} />
+            </View>
+        </ScrollView>
+    )
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    image: {
+        width: 88,
+        height: 88,
+        borderRadius: 50,
+        borderWidth: 2,
+        borderColor: '#fff'
+    },
+    Textname: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#fff',
+    }
+})
